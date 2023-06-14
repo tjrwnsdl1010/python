2c54f8a9a784cb44a3067d18db11beaa6603e7d5
tjrwnsdl1010 <aodxo1010@naver.com>
2023-06-14 11:47:27 +0900

    .

--- 3_function.py
+++ 3_function.py
@@ -1,10 +1,10 @@
 # 함수
 
-height = 100
-width = 50
+# height = 100
+# width = 50
 # 사각형의 들레: 300, 넓이 500입니다.
-permeter = (height+width)*2
-area = height*width
+# permeter = (height+width)*2
+# area = height*width
 # print(f"사각형의 둘레: {permeter}, 넓이 {area}입니다")
 
 
@@ -82,27 +82,27 @@ area = height*width
 #   return
 
 
-def my_max(*numbers):
-    result = 0
+# def my_max(*numbers):
+#     result = 0
     
 
-    for idx, number in enumerate(numbers):
-    #만약 for문에 첫번째 라면 제일 큰수로 설정
-        if idx == 0:
-            result = number
-        else:
-            if result < number:
-                result = number
-    return result
+#     for idx, number in enumerate(numbers):
+#     #만약 for문에 첫번째 라면 제일 큰수로 설정
+#         if idx == 0:
+#             result = number
+#         else:
+#             if result < number:
+#                 result = number
+#     return result
 
-    return result
+#     return result
         
     
-result = my_max(1,2,3,4,5)
-print(result)
+# result = my_max(1,2,3,4,5)
+# print(result)
 
-result = my_max(-1,-2,-3,-4,-5)
-print(result)
+# result = my_max(-1,-2,-3,-4,-5)
+# print(result)
 
 # 5.4 정의되지 않는 키워드 인자 처리
 
@@ -130,3 +130,91 @@ print(result)
 #     "password_confirm" : "12345"
 # }
 # user(**my_user)
+
+
+# 6. lambda
+# lambda parameter : expreesion
+
+# def my_sum(a,b):
+#     return a+b
+# result = my_sum(1,5)
+# print(result)
+
+
+# print((lambda a,b: a+b)(1,2))
+
+
+# def make_incrementor(n):
+#     return lambda x : x + n
+
+# f = make_incrementor(42)
+
+# f = lambda x : x+42
+
+# print(f(100))
+
+# 7. 이름공간 및 스코프(scope)
+
+# 1. local : 정의된함수 내부
+#   - 함수가 실행된 시점 이후부터 리턴 할 때 까지 존내
+# 2. enclosed : 상위 함수
+#   - 함수가 실행된 시점 이후부터 리턴 할 때 까지 존내
+# 3. global : 함수 밖 or import 된 모듈
+#   - 모듈이 호출된 시점 이후 끝까지 존재
+# 4. built-in : 파이썬에 내장되어있는 함수 혹은 변수
+#   - 파이썬이 실행된 이후 끝까지 존재
+
+# a = 1 #전역변수
+# def localscope(a): #지역변수 local
+#     print(a)
+# localscope(10)
+
+# global_a = 1
+# def localscope2():
+#     global global_a
+#     global_a = 2
+#     print(global_a)
+
+# localscope()
+# print(global_a)
+
+# 8. 재귀함수
+
+# 팩토리얼
+
+# def fact(n):
+#     result = 1
+#     while n > 1:
+#         result = result*n
+#         n -= 1
+#     return result
+        
+# print(fact(5))
+
+# 팩토리얼 재귀
+# def factorial(n):
+#     if n <= 1:
+#         return n
+#     else:
+#         return n* factorial(n-1)
+
+# print(factorial(5))
+
+# 피보나치 수열 반복
+# def fib(n):
+#     result = [1,1]
+#     for i in range(1,n):
+#         end1 = result[-1]
+#         end2 = result[len(result) -2]
+#         fib_num = end1 + end2
+#         result.append(fib_num)
+#     return result[-1]
+# print(fib(10))
+
+# 피보나치 수열 재귀 : 반복보다 연산량이 많음
+def fib(n):
+    if n == 1 or n == 0:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+print(fib(10))
---
+++ 4_dara_structure.py
@@ -0,0 +1,6 @@
+# 1. 문자열 메소드
+
+# .capitalize()
+greeting  = 'hello my name is seokjun'
+print(greeting.capitalize())
+print(greeting)
